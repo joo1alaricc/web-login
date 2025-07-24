7759b62789916accea47f5ad34d734d138203cae
joo1alaricc <erlicjonathanalaric@gmail.com>
2025-07-24 22:24:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     GITHUB_DATA_URL: 'https://raw.githubusercontent.com/joo1alaricc/dimasnathan/main/erlic.json',
     GITHUB_STATUS_URL: 'https://api.github.com/repos/joo1alaricc/dimasnathan/contents/status.json',
-    GITHUB_TOKEN: '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', 
+    GITHUB_TOKEN: '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', 
     BACKGROUND_IMAGE: '', 
     SUCCESS_AUDIO: '' 
 };
