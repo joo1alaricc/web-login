d203b080741578d7b85c96b3aba32f1e52c3daee
joo1alaricc <erlicjonathanalaric@gmail.com>
2025-07-24 22:10:30 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+
+const CONFIG = {
+    GITHUB_DATA_URL: 'https://raw.githubusercontent.com/joo1alaricc/dimasnathan/main/erlic.json',
+    GITHUB_STATUS_URL: 'https://api.github.com/repos/joo1alaricc/dimasnathan/contents/status.json',
+    GITHUB_TOKEN: '', 
+    BACKGROUND_IMAGE: '', 
+    SUCCESS_AUDIO: '' 
+};
