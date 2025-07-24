e98289d90b2314d8e89589ba49a58482a3a6aa76
joo1alaricc <erlicjonathanalaric@gmail.com>
2025-07-24 22:44:19 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// config.js
+if (typeof CONFIG === 'undefined') {
+    const CONFIG = {
+        GITHUB_DATA_URL: 'https://raw.githubusercontent.com/joo1alaricc/dimasnathan/main/erlic.json',
+        GITHUB_STATUS_URL: 'https://api.github.com/repos/joo1alaricc/dimasnathan/contents/status.json',
+        GITHUB_TOKEN: '', // Isi dengan token GitHub Anda
+        BACKGROUND_IMAGE: '', // Isi dengan URL gambar latar belakang
+        SUCCESS_AUDIO: '' // Isi dengan URL audio sukses
+    };
+    
+    // Make CONFIG globally available
+    window.CONFIG = CONFIG;
+}
