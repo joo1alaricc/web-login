545628d950099dba6c772dcbd7656dd23b4d81f3
joo1alaricc <erlicjonathanalaric@gmail.com>
2025-07-24 22:44:04 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-
-const CONFIG = {
-    GITHUB_DATA_URL: 'https://raw.githubusercontent.com/joo1alaricc/dimasnathan/main/erlic.json',
-    GITHUB_STATUS_URL: 'https://api.github.com/repos/joo1alaricc/dimasnathan/contents/status.json',
-    GITHUB_TOKEN: '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', 
-    BACKGROUND_IMAGE: '', 
-    SUCCESS_AUDIO: 'https://files.catbox.moe/my4nqf.mp3' 
-};
